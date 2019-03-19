639823961131e2de2b7ee18831359ad9afb43476
Alan Chu <alan_j_chu@yahoo.com>
2019-03-19 09:02:28 -0700

    Got rid of the .swp extensions

---
+++ character/race.py
@@ -0,0 +1,4 @@
+#Not exactly sure how I want to do this, but I guess a class that contains the
+#base characteristics, individual races, and a list of them. So bascially,
+#atomic, molecular, element type of data organization
+
