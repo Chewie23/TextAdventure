7f587a0009c267abf67c2ee7b4744ab1f98111ee
Alan 'Chewie' Chu <achu@grailbio.com>
2019-06-13 18:46:01 -0700

    Add generic creature stats

---
+++ character/generic_creature.py
@@ -0,0 +1,13 @@
+class Creature():
+    def __init__(self, STR, DEX, CON, INT, WIS, CHA):
+        self.str = STR
+        self.dex = DEX
+        self.con = CON
+        self.int = INT
+        self.wis = WIS
+        self.cha = CHA
+
+
+if __name__ == "__main__":
+    print("hello")
+
