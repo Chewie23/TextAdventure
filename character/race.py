f60594e1197bdab1dab095265522774d5ef02749
Chewie <alan_j_chu@yahoo.com>
2019-07-14 22:03:11 +0100

    Add StarFinder Class Android; Add notes for data organization

--- character/race.py
+++ character/race.py
@@ -1,6 +1,12 @@
-#Not exactly sure how I want to do this, but I guess a class that contains the
-#base characteristics, individual races, and a list of them. So bascially,
-#atomic, molecular, element type of data organization
+#TODO
+#Organize the structure of data
+#   Do I want one abstract class that all SF_classes rely on?
+#   Have each SF_class hold its own data, like below?
+#   Have on monolithic class that contains ALL SF_classes?
 
-class Race():
-    
+class Android():
+    def __init__(self):
+        self.stat_changes["DEX"] = 2
+        self.stat_changes["INT"] = 2
+        self.stat_changes["CHA"] = -2
+        self.hp = 4
