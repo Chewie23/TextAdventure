a06d20436fabfe59f8be67fd28edaff9f777c453
Chewie <alan_j_chu@yahoo.com>
2019-07-17 05:01:46 +0100

    Add basic SF class stats

--- character/race.py
+++ character/race.py
@@ -1,12 +1,71 @@
 #TODO
-#Organize the structure of data
-#   Do I want one abstract class that all SF_classes rely on?
-#   Have each SF_class hold its own data, like below?
-#   Have on monolithic class that contains ALL SF_classes?
+#Include special racial feats/traits
 
-class Android():
+class GenericRace():
+    def __init__(self):
+        self.stat_changes = {}
+        self.size = "Medium"
+        self.type = "Humanoid"
+        self.hp = 4
+
+class Android(GenericRace):
     def __init__(self):
         self.stat_changes["DEX"] = 2
         self.stat_changes["INT"] = 2
         self.stat_changes["CHA"] = -2
-        self.hp = 4
+        self.subtype = "Android"
+        
+class Human(GenericRace):
+    def __init__(self):
+        #+2 to any trait, so thus need to incorporate that
+        self.subtype = "Human"
+        
+class Kasatha(GenericRace):
+     def __init__(self):
+        self.stat_changes["STR"] = 2
+        self.stat_changes["WIS"] = 2
+        self.stat_changes["INT"] = -2
+        self.subtype = "Kasatha"
+        
+class Lashunta(GenericRace):
+    def __init__(self):
+        self.stat_changes["CHA"] = 2
+        self.subtype = "Lashunta"
+     
+class KorashaLashunta(Lashunta):
+    def __init__(self):
+        self.stat_changes["STR"] = 2
+        self.stat_changes["WIS"] = -2
+        self.sub_subtype = "Korasha"
+
+class DamayaLashunta(Lashunta):
+    def __init__(self):
+        self.stat_changes["INT"] = 2
+        self.stat_changes["CON"] = -2
+        self.sub_subtype = "Damaya"
+        
+class Shirrens(GenericRace):
+    def __init__(self):
+        self.stat_changes["CON"] = 2
+        self.stat_changes["WIS"] = 2
+        self.stat_changes["CHA"] = -2
+        self.hp = 6
+        self.subtype = "Shirren"
+        
+class Vesk(GenericRace):
+    def __init__(self):
+        self.stat_changes["STR"] = 2
+        self.stat_changes["CON"] = 2
+        self.stat_changes["INT"] = -2
+        self.hp = 6
+        self.subtype = "Vesk"
+        
+
+class Ysoki(GenericRace):
+    def __init__(self):
+        self.stat_changes["DEX"] = 2
+        self.stat_changes["INT"] = 2
+        self.stat_changes["STR"] = -2
+        self.hp = 2
+        self.size = "Small"
+        self.subtype = "Ysoki"        
