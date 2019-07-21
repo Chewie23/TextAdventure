14652982d6e016d66c62274f2a5b444f11dd2231
Chewie <alan_j_chu@yahoo.com>
2019-07-20 23:15:03 -0700

    Format dictionary with spaces

--- scripts/Trello_Kanban/practice.py
+++ scripts/Trello_Kanban/practice.py
@@ -13,7 +13,7 @@ base_url = "https://api.trello.com/1"
 url = '{}/members/me/boards'.format(base_url)
 
 #Fun fact: Trello API depends on authorization via params, not headers
-params_key_and_token = {'key':api_key,'token':api_token}
+params_key_and_token = {'key': api_key,'token': api_token}
 arguments = {'fields': 'name', 'lists': 'open'}
 
 response = requests.get(url=url, params=params_key_and_token, data=arguments)
